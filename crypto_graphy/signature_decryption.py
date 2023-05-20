7631e8e74385c7bfeeabe86ef55b844fd2636d71
prabhu <prabhun.is19@bmsce.ac.in>
2023-05-20 18:55:21 +0530

    3

--- crypto_graphy/signature_decryption.py
+++ crypto_graphy/signature_decryption.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.backends import default_backend
 import base64, os
 
-def decrypt_signature(encrypted_data, private_key_path = "crypto_graphy/private_key.pem"):
+def decrypt_signature(encrypted_data, private_key_path = "E:/final project/server2/crypto_graphy/private_key.pem"):
 
     # Decode the Base64-encoded encrypted data
     encrypted_data = base64.b64decode(encrypted_data)
@@ -25,3 +25,8 @@ def decrypt_signature(encrypted_data, private_key_path = "crypto_graphy/private_
     )
     
     return decrypted_data.decode("utf-8")
+
+text = "IDivPFVgTKcEMBWLZvf4wRtN4zSo9C5N4JcLSUj0NwOsaxTgz5yLPeMA4rbwnYA0noQC87o+SrUXfA2BgqYLVtI3TnU7yD10102lzvSpciu/qO4wQ88NzwEMbaKBdiysU20SkUOBAA8JtoLL5VQ64MwXMqf1kH/wWcG2b0XnnvCmYTRP6VgcgxCKgQrxqYgjloD06TmRPjQECX/kmxijUzK8nWGbJs+EkTk118PW16XbzKXrJ8aRKFyrAhZyDo/+h5PAOo15WLVY4pMDVtMmxqy188JkDGSelMKYGbK/1ARZNqwsBuvH7WDGtnh9xkXTHsfIp61zXfTiXlzsNomIFA=="
+
+x=decrypt_signature(text)
+print(x)
